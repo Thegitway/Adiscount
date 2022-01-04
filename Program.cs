3f2265578d3d28f7cb182bf96bccb5df2df516e1
OUKIL omar <omaroukil31@gmail.com>
2022-01-05 00:44:41 +0100

    connectionString

--- Program.cs
+++ Program.cs
@@ -1,19 +1,16 @@
 using Adiscount.db.mariaDb;
-using Adiscount.Entities;
-using Adiscount.repositories;
 using Adiscount.Services;
 using Microsoft.EntityFrameworkCore;
-string connectionString = "server=localhost;port=3306;database=omar;user=root;password=OUKIL";
-
 
 var builder = WebApplication.CreateBuilder(args);
 //dependency injection
+var connectionString = builder.Configuration.GetConnectionString("mariaDB");
+
 builder.Services.AddDbContext<MariaDbContext>(opt =>
-    opt.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString)));
+    opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddScoped<ClientService>();
 builder.Services.AddScoped<PictureService>();
-
 
 
 builder.Services.AddControllersWithViews();
@@ -23,9 +20,9 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-
     SeedData.Initialize(services);
 }
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -36,7 +33,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseCors(policy =>
     policy.AllowAnyOrigin()
-        );
+);
 
 app.MapControllerRoute(
     "default",
